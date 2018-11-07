c10ff4af683b29ea9eb91b4b9117a0f1f7dc4f76
xheartean <gokhan_koc@windowslive.com>
2018-11-07 15:12:18 +0200

    Throttling added

---
+++ lazyload.js
@@ -0,0 +1,51 @@
+// Execute Once When Window Loaded
+window.onload = lazyLoad;
+
+// Executes Throttle Function Once (which returns another function)
+window.onscroll = throttle(function (){
+    lazyLoad();
+},300);
+
+window.onresize = throttle(function (){
+    lazyLoad();
+},300);
+
+// Throttle Scroll Event
+function throttle(callback, limit){
+    let throttling = false;
+    return function(){
+        // UNTHROTTLED PART!
+        if(!throttling){
+            callback.call();
+            throttling = true;
+            setTimeout(function (){
+                throttling = false;
+            }, limit);
+        }
+    }
+}
+
+function lazyLoad(){
+    //Select All Images with "lazy" Class
+    let lazyImages = document.querySelectorAll("img.lazy");
+
+    //Iterate Through the Array to change SRC with DATA-SRC
+    for(let i=0; i<lazyImages.length; i++){
+
+        //Check if the Image is in Viewport
+        if(elementInViewport(lazyImages[i])){
+            lazyImages[i].setAttribute('src', lazyImages[i].getAttribute('data-src'));
+            lazyImages[i].classList.remove('lazy');
+        }
+    }
+}
+
+// This function is checking if the element is in viewport
+function elementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+
+    return (
+        rect.top <= (window.innerHeight) &&
+        rect.right <= (window.innerWidth)
+    )
+}
